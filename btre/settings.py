8d1a17958ae0b8c8e9f7459a355e2944e8b65a35
BisiOlaYemi <ogunrinde_olayemi@yahoo.com>
2023-04-07 13:55:29 -0700

    Code commit

--- btre/settings.py
+++ btre/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-e+xv#*_p&17aq%tv(9a&td-t-71og$h1$n-(7)9k^gf9t5n!m0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
