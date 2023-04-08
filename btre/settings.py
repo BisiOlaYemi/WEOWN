689c5537e8ad0ee00e777cd5ddaf8af01f006bed
BisiOlaYemi <ogunrinde_olayemi@yahoo.com>
2023-04-08 07:35:51 -0700

    changes made

--- btre/settings.py
+++ btre/settings.py
@@ -11,9 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import os 
-import dj_database_url
-from decouple import config
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -96,7 +94,6 @@ DATABASES = {
     }
 }
 
-DATABASES['default'] =dj_database_url.config()
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
