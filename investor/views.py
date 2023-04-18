d1b783ed2070a5a8ceb1f1d09f2536fb1a8139ec
BisiOlaYemi <ogunrinde_olayemi@yahoo.com>
2023-04-18 09:41:04 -0700

    updating Investor dashboard

--- investor/views.py
+++ investor/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
-from django.core.mail import send_mail
+from django.core.mail import send_mail, EmailMessage
 from django.apps import apps
 from investor.models import Investor
 from django.contrib import messages
 from django.contrib.auth import authenticate, login
+from django.conf import settings
 
 def signup(request):
     Investor = apps.get_model('investor', 'Investor')
@@ -49,3 +50,31 @@ def signin(request):
             messages.error(request, 'Email or password is incorrect.')
 
     return render(request, 'investor.html')
+
+
+def get_approved(request):
+    success_message = ''
+    if request.method == 'POST':
+        immigration_status = request.POST.get('immigration-status')
+        full_address = request.POST.get('full-address')
+        employment_status = request.POST.get('employment-status')
+        credit_score = request.FILES.get('credit-score')
+
+        # Send email with details
+        email = EmailMessage(
+            'New investor application',
+            f'Immigration status: {immigration_status}\nLocation address: {full_address}\nEmployment status: {employment_status}',
+            settings.EMAIL_HOST_USER,
+            ['frankolayemi@gmail.com'],
+        )
+        email.attach(credit_score.name, credit_score.read(), credit_score.content_type)
+        email.send()
+
+        # Add success message to context
+        success_message = 'Your details have been sent and are awaiting approval.'
+
+    return render(request, 'investor/application.html', {'success_message': success_message})
+
+
+
+
